cf124815ddb34711647d2838aead3f3932ddbef4
sobak3n <enchantressimba@gmail.com>
2023-10-01 22:02:08 +0300

    new commit

--- test.cpp
+++ test.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int main(int argc , char* argv[]){
     cout<<"lol"<<argv[0]<<endl;
+    cout<<"lol"<<argv[0]<<endl;
+    cout<<"lol"<<argv[0]<<endl;
+    cout<<"lol"<<argv[0]<<endl;
+    cout<<"lol"<<argv[0]<<endl;
+    cout<<"lol"<<argv[0]<<endl;
+    cout<<"lol"<<argv[0]<<endl;
+    
 }
